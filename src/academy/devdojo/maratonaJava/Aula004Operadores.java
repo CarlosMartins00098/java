52166792646bdf84b5b886e371813fb72620a92f
carlosmartins00098 <carlosmartins00098@gmail.com>
2025-10-21 20:57:06 -0300

    add aula04

---
+++ src/academy/devdojo/maratonaJava/Aula004Operadores.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonaJava;
+
+public class Aula004Operadores {
+    public static void main(String[] args) {
+
+        int num01 = 10;
+        int num02 = 20;
+        System.out.println(num01+num02);
+}
+}
