7ec73a66addf263a09eaa5638facf643ddbc3398
carlosmartins00098 <carlosmartins00098@gmail.com>
2025-10-21 21:10:43 -0300

    atualização aula04

--- src/academy/devdojo/maratonaJava/Aula004Operadores.java
+++ src/academy/devdojo/maratonaJava/Aula004Operadores.java
@@ -3,8 +3,28 @@ package academy.devdojo.maratonaJava;
 public class Aula004Operadores {
     public static void main(String[] args) {
 
+        // + - / *
         int num01 = 10;
         int num02 = 20;
         System.out.println(num01+num02);
-}
+
+        // %
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        //< > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+
+        System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte "+isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte "+isDezIgualVinte);
+        System.out.println("isDezIgualDez "+isDezIgualDez);
+        System.out.println("isDezDiferenteDez "+isDezDiferenteDez);
+
+
+    }
 }
